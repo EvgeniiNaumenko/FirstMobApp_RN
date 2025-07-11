7d472c11f9d097e3ff0f327b44e6916258646c46
EvgeniiNaumenko <evgeniynaumenko2017@gmail.com>
2025-07-11 16:14:40 +0300

    add sub div mul

--- src/pages/calc/Calc.tsx
+++ src/pages/calc/Calc.tsx
@@ -5,36 +5,273 @@ import { useState } from "react";
 const maxResultDigits = 5;
 
 export default function Calc() {
-  const[result, setResult] = useState("0");
-  const{width, height}=useWindowDimensions();
+  const {width, height}=useWindowDimensions();
+  const [result, setResult] = useState("0");
+  const [memory, setMemory] = useState("0");
+  const [history, setHistory] = useState("");
+  const [calculation, setCalculation] =useState("");
 
-  const onOperationPress = (title:string, data?:string)=>{
-    switch(data){
-      case "clear" : setResult("0"); break;
-      case "backspace" :
-        if(result.length>1)
-        {
-          setResult(result.substring(0, result.length-1));
-        }
-        else
-        {
-          setResult("0");
-        }
-       break;
-       case "inverse" : setResult((1/Number(result)).toString());
-       break;
+  const onMemoryOperationPress = (title:string, data?:string)=>{
+    switch(title){
+      case "MC" :
+        setMemory("0")
+        break;
+      case "MR" :
+        setResult(memory);
+        break;
+      case "M+" :
+        setMemory((Number(memory)+Number(result)).toString());
+        break;
+      case "M-" :
+        setMemory((Number(memory)-Number(result)).toString());
+        break;
+      case "MS" :
+        setMemory(result);
+        break;
+      case "Mv" :
+        //TODO
+        break;
+    }
+  };
+  const getSecondOperand = (expr:String)=> {
+  // Удаляем "=" на конце
+  expr = expr.trim().replace(/=$/, "");
+
+  // Ищем знак между числами (поддержка + - × ÷)
+  const match = expr.match(/(.+)([+\-×÷])\(?(-?\d+)\)?$/);
+
+  if (match) {
+    let operator = match[2]; // +, -, ×, ÷
+    let operand = match[3];  // уже содержит минус если был
+
+    if (operator === '+') return operand.startsWith('-') ? operand : '+' + operand;
+    if (operator === '-') return operand.startsWith('-') ? operand : '-' + operand;
+    if (operator === '×' || operator === '÷') return operand;
+  }
+
+  return null; // если не удалось разобрать
+  }
+  const onOperationPress = (title: string, data?: string) => {
+  switch (data) {
+    case "clear":{
+      setResult("0");
+      setHistory("");
+      setCalculation("");
+      break;
+    }
+    case "clearResult":{
+      setResult("0");
+      break;
+    }
+    case "backspace":{
+      if (result.length > 1) {
+        setResult(result.substring(0, result.length - 1));
+      } else {
+        setResult("0");
       }
-    };
+      break;
+    }
+    case "inverse":{
+      setHistory("1/(" + Number(result).toString() + ")=");
+      var inverse = (1 / Number(result)).toString();
+      setResult(inverse);
+      setCalculation(inverse);
+      break;
+    }
+    case "add":{
+      if(history.slice(-1)!="+" && history !="" && history!.includes("+") && history.slice(-1)!="\u003D"){
+        setHistory(history.substring(0, history.length - 1)+"+");
+        break;
+      }
+
+      if(calculation !="" && result =="0"){
+        setHistory(calculation + "+" );
+        break;
+      }
+
+      if(calculation != "" && result != "0"){
+        let res = (Number(calculation)+Number(result)).toString()
+        setCalculation(res);
+        setHistory(res + "+");
+        setResult("0");
+        break;
+      }
+      setHistory(result + "+");
+      setCalculation(result);
+      setResult("0");
+      break;
+    }
+
+    case "sub":{
+      if(history.slice(-1)!="-" && history !="" && history!.includes("-") && history.slice(-1)!="\u003D"){
+        setHistory(history.substring(0, history.length - 1)+"-");
+        break;
+      }
+
+      if(calculation !="" && result =="0"){
+        setHistory(calculation + "-" );
+        break;
+      }
+
+      if(calculation != "" ){
+        let res = (Number(calculation)-Number(result)).toString()
+        setCalculation(res);
+        setHistory(res + "-");
+        setResult("0");
+        break;
+      }
+
+      setHistory(result + "-");
+      setCalculation(result);
+      setResult("0");
+      break;
+    }
+    
+    case "mul":{
+      console.log(calculation);
+      if(history.slice(-1)!="\u00D7" && history !="" && history!.includes("\u00D7") && history.slice(-1)!="\u003D"){
+        setHistory(history.substring(0, history.length - 1)+"\u00D7");
+        break;
+      }
+      if(calculation !="" && result =="0"){
+        setHistory(calculation + "\u00D7" );
+        break;
+      }
+
+      if(calculation != "" ){
+        let res = (Number(calculation)*Number(result)).toString()
+        setCalculation(res);
+        setHistory(res + "\u00D7");
+        setResult("0");
+        break;
+      }
+
+      setHistory(result + "\u00D7");
+      setCalculation(result);
+      setResult("0");
+      break;
+     }
+    case "div":{
+      if(history.slice(-1)!="\u00F7" && history !="" && history!.includes("+") && history.slice(-1)!="\u003D"){
+        setHistory(history.substring(0, history.length - 1)+"\u00F7");
+        break;
+      }
+
+      if(calculation !="" && result =="0"){
+        setHistory(calculation + "\u00F7" );
+        break;
+      }
+
+      if(calculation != "" ){
+        let res = (Number(calculation)/Number(result)).toString()
+        setCalculation(res);
+        setHistory(res + "\u00F7");
+        setResult("0");
+        break;
+      }
+
+      setHistory(result + "\u00F7");
+      setCalculation(result);
+      setResult("0");
+      break; 
+    } 
+    case "square":{
+      let res = (Math.pow(Number(result),2)).toString();
+      setCalculation(res);
+      setResult(res);
+      setHistory(result+"\u00B2")
+      break;
+    }
+    case "sqrt":{
+      if(Number(result)<0) break;
+      
+      let res =Math.sqrt(Number(result)).toString();
+      setCalculation(res);
+      setResult(res);
+      setHistory("\u00B2\u221A"+result)
+      break; 
+    }
+    case "percent":{
+      if(!history || !calculation) break;
+      let operator = history.slice(-1);
+      let left = Number(calculation);
+      let right = Number(result);
+      let res = "0";
+
+      switch (operator) {
+        case "+":
+          res = (left + ((left*right)/100)).toString();
+          break;
+        case "-":
+          res = (left - ((left*right)/100)).toString();
+          break;
+        case "\u00D7":
+          res = (left * ((left*right)/100)).toString();
+          break;
+        case "\u00F7":
+          res = right !== 0 ? (left / ((left*right)/100)).toString() : "Error";
+          break;
+        default:
+          res = result;
+      }
+      setHistory(history + (Number(result)/100).toString() + "\u003D");
+      setResult(res);
+      setCalculation(res);
+      break; 
+    }
+    case "equal":{
+
+      if (!calculation || !history || history.includes("\u003D")) return;
+
+      let operator = history.slice(-1);
+      let left = Number(calculation);
+      let right = Number(result);
+      let res = "0";
+
+      switch (operator) {
+        case "+":
+          res = (left + right).toString();
+          break;
+        case "-":
+          res = (left - right).toString();
+          break;
+        case "\u00D7":
+          res = (left * right).toString();
+          break;
+        case "\u00F7":
+          res = right !== 0 ? (left / right).toString() : "Error";
+          break;
+        default:
+          res = result;
+      }
+
+      setHistory(history + result + "\u003D");
+      setCalculation(res);
+      setResult("0");
+      break;
+    }
+    default:
+      break;
+  }
+};
 
   const onDigitPress = (title:string)=>{
-    if(result.length< (result.includes('.') ? maxResultDigits+1: maxResultDigits)){
+    let extraChars = 0;
+    if (result.includes('.')) extraChars++;
+    if (result.includes('-')) extraChars++;
+
+    if (history.includes("\u003D")){  
+      setCalculation("");
+      setHistory("0");
+    };
+
+    if (result.length < maxResultDigits + extraChars){
       if(result[0]=="0" && result[1] !="." ){
         setResult(title)
       }
       else{
         setResult(result+title)
       }
-      console.log(title);
     }
   }
   const onDotPress = (title:string)=>{
@@ -43,62 +280,63 @@ export default function Calc() {
     }
   }
   const onPmPress = (title:string)=>{
-    console.log(title);
+    result.includes("-") ? setResult(result.substring(1)) : setResult("-"+result);
   }
+
   const portraitView = () =>{
     return <View style={styles.calcContainer}>
       <Text style={styles.title}>Калькулятор</Text>
-      <Text style={styles.expression}>22+33=</Text>
-      <Text style={[styles.result, {fontSize : result.length < 20 ? styles.result.fontSize : styles.result.fontSize * 19 / result.length}]}>{result}</Text>
+      <Text style={styles.expression}>{history}</Text>
+      <Text style={[styles.result, {fontSize : result.length < 20 ? styles.result.fontSize : styles.result.fontSize * 19 / result.length}]}>{result == "0" ? calculation : result}</Text>
       <View style={styles.calcFirstRow}>
-      <CalcButton title="MC" action={onOperationPress}/>
-      <CalcButton title="MR" action={onOperationPress}/>
-      <CalcButton title="M+" action={onOperationPress}/>
-      <CalcButton title="M-" action={onOperationPress}/>
-      <CalcButton title="MS" action={onOperationPress}/>
-      <CalcButton title="Mv" action={onOperationPress}/>
+      <CalcButton title="MC" action={onMemoryOperationPress}/>
+      <CalcButton title="MR" action={onMemoryOperationPress}/>
+      <CalcButton title="M+" action={onMemoryOperationPress}/>
+      <CalcButton title="M-" action={onMemoryOperationPress}/>
+      <CalcButton title="MS" action={onMemoryOperationPress}/>
+      <CalcButton title="Mv" action={onMemoryOperationPress}/>
       </View>
 
       <View style={styles.calcButtonRow}>
-        <CalcButton title="%"         action={onOperationPress}/>
-        <CalcButton title="CE"        action={onOperationPress}/>
+        <CalcButton title="%"         action={onOperationPress} data="percent"/>
+        <CalcButton title="CE"        action={onOperationPress} data="clearResult"/>
         <CalcButton title="C"         action={onOperationPress} data="clear"/>
         <CalcButton title={"\u232B"}  action={onOperationPress} data="backspace"/>
       </View>
 
       <View style={styles.calcButtonRow}>
-        <CalcButton title={'\u00B9/\u{1D465}'} action={onOperationPress} data="inverse" />
-        <CalcButton title={'\u{1D465}\u00B2'} action={onOperationPress} data="square"/>
-        <CalcButton title={'\u221A\u{1D465}\u0305'}action={onOperationPress} data="sqrt" />
-        <CalcButton title={'\u00F7'}  action={onOperationPress} data="div"/>
+        <CalcButton title={'\u00B9/\u{1D465}'}      action={onOperationPress} data="inverse" />
+        <CalcButton title={'\u{1D465}\u00B2'}       action={onOperationPress} data="square"/>
+        <CalcButton title={'\u221A\u{1D465}\u0305'} action={onOperationPress} data="sqrt" />
+        <CalcButton title={'\u00F7'}                action={onOperationPress} data="div"/>
       </View>
 
       <View style={styles.calcButtonRow}>
         <CalcButton title={"7"} type ="digit" action={onDigitPress}/>
         <CalcButton title={"8"} type ="digit" action={onDigitPress}/>
         <CalcButton title={"9"} type ="digit" action={onDigitPress}/>
-        <CalcButton title={'\u00D7'} action={onOperationPress} data="mul"/>
+        <CalcButton title={'\u00D7'}          action={onOperationPress} data="mul"/>
       </View>
 
       <View style={styles.calcButtonRow}>
         <CalcButton title={"4"} type ="digit" action={onDigitPress}/>
         <CalcButton title={"5"} type ="digit" action={onDigitPress}/>
         <CalcButton title={"6"} type ="digit" action={onDigitPress}/>
-        <CalcButton title={'\uFF0D'} action={onOperationPress} data="sub"/>
+        <CalcButton title={'\uFF0D'}          action={onOperationPress} data="sub"/>
       </View>
 
       <View style={styles.calcButtonRow}>
         <CalcButton title={"1"} type ="digit" action={onDigitPress}/>
         <CalcButton title={"2"} type ="digit" action={onDigitPress}/>
         <CalcButton title={"3"} type ="digit" action={onDigitPress}/>
-        <CalcButton title={'\uFF0B'} action={onOperationPress} data="add" />
+        <CalcButton title={'\uFF0B'}          action={onOperationPress} data="add" />
       </View>
 
       <View style={styles.calcButtonRow}>
-        <CalcButton title={"\u00B1"} type ="digit" action={onPmPress}/>
-        <CalcButton title={"0"} type ="digit" action={onDigitPress}/>
-        <CalcButton title={"."} type ="digit" action={onDotPress}/>
-        <CalcButton title={"\u003D"} type ="equal" action={onOperationPress}/>
+        <CalcButton title={"\u00B1"}  type ="digit" action={onPmPress}/>
+        <CalcButton title={"0"}       type ="digit" action={onDigitPress}/>
+        <CalcButton title={"."}       type ="digit" action={onDotPress}/>
+        <CalcButton title={"\u003D"}  type ="equal" action={onOperationPress} data="equal"/>
       </View>
     </View>
   };
@@ -143,20 +381,20 @@ export default function Calc() {
 
       <View style={styles.calcButtonRow}>
         <CalcButton title={'\u{1D465}\u00B2'} action={onOperationPress} data="square"/>
-        <CalcButton title="1" type="digit"  action={onDigitPress}/>
-        <CalcButton title="2" type="digit"  action={onDigitPress}/> 
-        <CalcButton title="3" type="digit"  action={onDigitPress}/> 
-        <CalcButton title={'\uFF0D'} action={onOperationPress} data="sub"/>
-        <CalcButton title="CE"       action={onOperationPress} data="clearEntry"/> 
+        <CalcButton title="1" type="digit"    action={onDigitPress}/>
+        <CalcButton title="2" type="digit"    action={onDigitPress}/> 
+        <CalcButton title="3" type="digit"     action={onDigitPress}/> 
+        <CalcButton title={'\uFF0D'}          action={onOperationPress} data="sub"/>
+        <CalcButton title="CE"                action={onOperationPress} data="clearEntry"/> 
       </View>
 
       <View style={styles.calcButtonRow}>
-        <CalcButton title={'\u221A\u{1D465}\u0305'}action={onOperationPress} data="sqrt" />
-        <CalcButton title={'\u00B1'} type="digit" action={onPmPress}/>
-        <CalcButton title="0" type="digit"        action={onDigitPress}/> 
-        <CalcButton title="," type="digit"        action={onDotPress}/> 
-        <CalcButton title={'\uFF0B'} action={onOperationPress} data="add" />
-        <CalcButton title={'\uFF1D'} type="equal" action={onOperationPress}/>
+        <CalcButton title={'\u221A\u{1D465}\u0305'}               action={onOperationPress} data="sqrt" />
+        <CalcButton title={'\u00B1'}  type="digit"                action={onPmPress}/>
+        <CalcButton title="0"         type="digit"                action={onDigitPress}/> 
+        <CalcButton title=","         type="digit"                action={onDotPress}/> 
+        <CalcButton title={'\uFF0B'}  action={onOperationPress}   data="add" />
+        <CalcButton title={'\uFF1D'}  type="equal"                action={onOperationPress}/>
       </View>
     </View>;
   };
--- src/pages/game/Game.tsx
+++ src/pages/game/Game.tsx
@@ -1,5 +1,5 @@
 import { useContext, useState } from "react";
-import { Pressable, StyleSheet, Text, TouchableWithoutFeedback, View } from "react-native";
+import { Pressable, StyleSheet, Text, TouchableWithoutFeedback, useWindowDimensions, View } from "react-native";
 import { AppContext } from "../../shared/context/appContext";
 
 //swipes - жесты проведения с ограничением минимальных дистаний и скоростей
@@ -12,13 +12,84 @@ type eventData = {
 const distanceThreshold = 50; // минимальная длинна свайпа
 const timeThreshold = 500; // максимальный время свайпа
 
+function tileBackground(tileValue: number){
+    return tileValue == 0?       "#BDAFA2"
+    : tileValue == 2     ?       "#EEE3D8"
+    : tileValue == 4     ?       "#EEE1D0"
+    : tileValue == 8     ?       "#E8B486"
+    : tileValue == 16    ?       "#E79B73"
+    : tileValue == 32    ?       "#E4846E"
+    : tileValue == 64    ?       "#E26A51"
+    : tileValue == 128   ?       "#444"
+    : tileValue == 256   ?       "#444"
+    : tileValue == 512   ?       "#444"
+    : tileValue == 1024  ?       "#444"
+    : tileValue == 2048  ?       "#444"
+    : tileValue == 4096  ?       "#444"
+    :                            "#444";
+
+}
+function tileForeground(tileValue: number){
+    return tileValue == 0?       "#BDAFA2"
+    : tileValue == 2     ?       "#746C63"
+    : tileValue == 4     ?       "#766E66"
+    : tileValue == 8     ?       "#FAF3EF"
+    : tileValue == 16    ?       "#FBF5F2"
+    : tileValue == 32    ?       "#FBF5F2"
+    : tileValue == 64    ?       "#FBF5F2"
+    : tileValue == 128   ?       "#fff"
+    : tileValue == 256   ?       "#fff"
+    : tileValue == 512   ?       "#fff"
+    : tileValue == 1024  ?       "#fff"
+    : tileValue == 2048  ?       "#fff"
+    : tileValue == 4096  ?       "#fff"
+    :                            "#fff";
+
+}
+
+
 export default function Game(){
 
     const {navigate} = useContext(AppContext)
     const [text, setText] = useState("Game");
-
+    const {width, height}=useWindowDimensions();
+    const [tiles, setTiles]= useState([
+        0,      2,      4,      8,
+        16,     32,     64,     128,
+        256,    512,    1024,   2048,
+        4096,   8192,   16384,  32768
+    ])
+    const tileFontSize = (tileValue: number) =>{
+    return tileValue < 10     ?   width *0.1 
+        : tileValue  < 100    ?   width *0.09
+        : tileValue  < 1000   ?   width *0.08
+        : tileValue  < 10000  ?   width *0.07
+        :                         width *0.06
+    }
 
     var startData: eventData | null = null;
+    // START GAME
+
+    const spawnTile = () =>{
+        var freeTiles = [];
+        for(let i=0; i < tiles.length; i +=1){
+            if(tiles[i]==0){
+                freeTiles.push(i)
+            }
+        }
+        const randomIndex = freeTiles[Math.floor(Math.random()*freeTiles.length)];
+        tiles[randomIndex] = Math.random()<0.9 ? 2 : 4
+        setTiles(tiles);
+    }
+    const newGame = () => {
+        for(let i=0; i < tiles.length; i +=1){
+            tiles[i]=0
+        }
+        spawnTile();
+        spawnTile();
+        setTiles([...tiles]);
+    }
+
     const detectSwipe = (finishData:eventData)=>{
         if(startData == null) return;
         const dx = finishData.x-startData!.x;
@@ -49,34 +120,139 @@ export default function Game(){
         }
     }
     
-    return <TouchableWithoutFeedback
-        onPressIn={e => {startData= {
-            x: e.nativeEvent.pageX,
-            y: e.nativeEvent.pageY,
-            t: e.nativeEvent.timestamp,
-        }}}
-
-        onPressOut={e=>detectSwipe({
-            x: e.nativeEvent.pageX,
-            y: e.nativeEvent.pageY,
-            t: e.nativeEvent.timestamp,
-        })}>
-
-        <View style={styles.container}>
-            <Pressable onPress={()=>navigate('calc')}>
-                <Text style={{fontSize:50}}>{text}</Text>
-            </Pressable>
+    return <View style={styles.container}>
+        <View style={[styles.topBlock, {width: width*0.95}]}>
+            <Text style={styles.topBlockText}>2048</Text>
+            <View style={styles.topBlockSub}>
+                <View style={styles.topBlockScores}>
+                    <View style={styles.topBlockScore}>
+                        <Text style={styles.topBlockScoreText}>SCORE</Text>
+                        <Text style={styles.topBlockScoreText}>10000</Text>
+                    </View>
+                    <View style={styles.topBlockScore}>
+                        <Text style={styles.topBlockScoreText}>BEST</Text>
+                        <Text style={styles.topBlockScoreText}>1005000</Text>
+                    </View>
+                </View>
+                <View style={styles.topBlockButtons}>
+                    <Pressable style={styles.topBlockButton} onPress={newGame}><Text style={styles.topBlockButtonText}>NEW</Text></Pressable>
+                    <Pressable style={styles.topBlockButton}><Text style={styles.topBlockButtonText}>UNDO</Text></Pressable>
+                </View>
+            </View>
         </View>
-    </TouchableWithoutFeedback>
+
+        <Text>Набери 2048</Text>
+
+        <TouchableWithoutFeedback
+            onPressIn={e => {startData= {
+                x: e.nativeEvent.pageX,
+                y: e.nativeEvent.pageY,
+                t: e.nativeEvent.timestamp,
+            }}}
+            
+            onPressOut={e=>detectSwipe({
+                x: e.nativeEvent.pageX,
+                y: e.nativeEvent.pageY,
+                t: e.nativeEvent.timestamp,
+            })}>
+
+            <View style={[styles.field, {width: width*0.95, height:width*0.95}]}>
+               {tiles.map((tile, index) => <Text key ={index} 
+               style={[styles.tile,{
+                backgroundColor: tileBackground(tile),
+                color: tileForeground(tile),
+                width: width*0.21,
+                height: width*0.21,
+                fontWeight: 900,
+                marginLeft: width*0.022,
+                marginTop: width*0.022,
+                fontSize: tileFontSize(tile),
+               } ]}>{tile}</Text>)}
+            </View>
+        </TouchableWithoutFeedback>
+        <Text>{text}</Text>
+    </View>
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex: 1,
+  container: {
+    backgroundColor: "#FCF7F0",
     display: "flex",
+    flex: 1,
     flexDirection: "column",
-    justifyContent: "center",
+    justifyContent: "flex-start",
     alignItems: "center",
-    backgroundColor: "#123321"
+    width: "100%"
+  },
+  topBlock: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  topBlockText: {
+    backgroundColor: "gold",
+    borderRadius: 7,
+    color: "white",
+    fontSize: 32,
+    marginVertical: 5,
+    paddingHorizontal: 10,
+    verticalAlign: "middle"
+  },
+  topBlockSub: {
+    display: "flex",
+    flex: 1,
+    flexDirection: "column",
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  topBlockScores: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  topBlockScore: {
+    backgroundColor: "#3C3A34",
+    borderRadius: 7,
+    flex: 1,
+    marginVertical: 5,
+    marginLeft: 10,
+    padding: 5,
+  },
+  topBlockScoreText: {
+    color: "white",
+    textAlign: "center"
+  },
+  topBlockButtons: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-between",
+
+  },
+  topBlockButton: {
+    backgroundColor: "#E06849",
+    borderRadius: 7,
+    flex: 1,
+    marginVertical: 5,
+    marginLeft: 10,
+    padding: 10,
+
+  },
+  topBlockButtonText: {
+    color: "white",
+    textAlign: "center"
+  },
+  field: {
+    borderRadius: 10,
+    backgroundColor: "#A29383",
+    marginHorizontal: "auto",
+    display: "flex",
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  tile:{
+    borderRadius: 5,
+    textAlign: "center",
+    verticalAlign: "middle",
+
   },
 });
